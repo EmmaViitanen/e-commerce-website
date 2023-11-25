1c4360daa40f63d25d5c5b95e1368a4e739d3969
Emma Viitanen <viem23ni@student.ju.se>
2023-11-25 17:43:07 +0100

    somewhat responsive

--- functions.js
+++ functions.js
@@ -8,3 +8,14 @@ function cardFunction() {
     x.style.display = "none";
   }
 }
+
+// document.getElementById("menu").addEventListener("click", menuFunction);
+
+function menuFunction() {
+  var x = document.getElementById("hamburger-menu");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
