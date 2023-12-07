e5e6b9a6767b89497a05a3af32a56ebc289f8146
olne23pf <nellieolssonwennberg@gmail.com>
2023-12-07 13:04:13 +0100

    page names and try to confetti ani

---
+++ javascript/confetti.js
@@ -0,0 +1,18 @@
+// go Buckeyes!
+
+(function confettianimation() {
+  confetti({
+    particleCount: 2,
+    angle: 60,
+    spread: 55,
+    origin: { x: 0 },
+    colors: colors,
+  });
+  confetti({
+    particleCount: 2,
+    angle: 120,
+    spread: 55,
+    origin: { x: 1 },
+    colors: colors,
+  });
+})();
